77a4c633be80f7fa1755275e58ccaf8e36ea217e
Dash <dkshowarth@gmail.com>
2023-07-13 20:14:07 -0700

    add function for requesting and displaying 5 day weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var searchInput = $('#weatherSearch');
 var historyList = $('#history');
-var forecast = $('#fiveDayForecast');
 var currentWeather = $('#currentWeather');
 
 var cityLon;
@@ -23,7 +22,7 @@ var getLocData = function(cityName){
     })
 }
 
-var getCurrentWeather = function(){
+var displayCurrentWeather = function(){
     //create api url from lat and long pulled by getLocData
     var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=7f9953f1dd73d072c914ff82ce5ca3d1';
 
@@ -34,12 +33,30 @@ var getCurrentWeather = function(){
     .then(function(data){
         // Update title, temperature, wind, and humidity
         currentWeather.children('h1').text('Today, ' + dayjs().format('MMMM D, YYYY') + ' the current weather for ' + data.name + ' is:');
-        currentWeather.children('ul').children('.temperature').text('Temp: ' + data.main.temp + '°F')
-        currentWeather.children('ul').children('.wind').text('Wind: ' + data.wind.speed + 'MPH')
-        currentWeather.children('ul').children('.humidity').text('Humidity: ' + data.main.humidity + '%')
+        currentWeather.children('ul').children('.temperature').text('Temp: ' + data.main.temp + '°F');
+        currentWeather.children('ul').children('.wind').text('Wind: ' + data.wind.speed + 'MPH');
+        currentWeather.children('ul').children('.humidity').text('Humidity: ' + data.main.humidity + '%');
     })
 }
 
-var displayWeather = function(){
+var displayWeatherForecast = function(){
 
+    var cityUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=7f9953f1dd73d072c914ff82ce5ca3d1';
+
+    fetch(cityUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        // Update title, temperature, wind, and humidity
+        for(var i = 0; i < 5; i++){
+            var forecastDay = $('#day' + (i+1));
+
+            forecastDay.children('h5').text(dayjs().format('MMMM D, YYYY'));
+            forecastDay.children('ul').children('.temperature').text('Temp: ' + data.list[i]["temp"]["day"] + '°F');
+            forecastDay.children('ul').children('.wind').text('Wind: ' + data.list[i]["temp"]["day"] + 'MPH');
+            forecastDay.children('ul').children('.humidity').text('Humidity: ' + data.list[i]["humidity"] + '%');
+        }
+
+    })
 }
