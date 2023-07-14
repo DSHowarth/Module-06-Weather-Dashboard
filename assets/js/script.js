6b31948d866668e6f222ecd4fab77742f55c108e
Dash <dkshowarth@gmail.com>
2023-07-14 16:25:28 -0700

    fix forecast request function. It now properly grabs data from the free api

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,6 @@ $( document ).ready(function(){
         })
         .then(function(data){
             // Update title, temperature, wind, and humidity
-            console.log(data)
             currentWeather.children('h1').text('Today, ' + dayjs().format('MMMM D, YYYY') + ' the current weather for ' + data.name + ' is:');
             currentWeather.children('ul').children('.temperature').text('Temp: ' + data.main.temp + '°F');
             currentWeather.children('ul').children('.wind').text('Wind: ' + data.wind.speed + 'MPH');
@@ -31,7 +30,7 @@ $( document ).ready(function(){
 
     var displayForecast = function(){
 
-        var cityUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=7f9953f1dd73d072c914ff82ce5ca3d1';
+        var cityUrl = 'https://api.openweathermap.org/data/2.5/forecast/?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&appid=7f9953f1dd73d072c914ff82ce5ca3d1';
 
         fetch(cityUrl)
         .then(function(response){
@@ -41,12 +40,18 @@ $( document ).ready(function(){
             // Update title, temperature, wind, and humidity
             console.log(data);
             for(var i = 0; i < 5; i++){
-                var forecastDay = $('#day' + (i+1));
-
-                forecastDay.children('h5').text(dayjs().format('MMMM D, YYYY'));
-                forecastDay.children('ul').children('.temperature').text('Temp: ' + data.list[i]["temp"]["day"] + '°F');
-                forecastDay.children('ul').children('.wind').text('Wind: ' + data.list[i]["temp"]["day"] + 'MPH');
-                forecastDay.children('ul').children('.humidity').text('Humidity: ' + data.list[i]["humidity"] + '%');
+                var forecastDay = $('#day' + (i+1)).children('.card-body');
+                var timeInterval = i;
+                if(i === 0){
+                    timeInterval = 5;
+                }
+                else{
+                    timeInterval = 5 + (i * 8);
+                }
+                forecastDay.children('h5').text(dayjs(data.list[timeInterval].dt_txt).format('MMMM D, YYYY'));
+                forecastDay.children('ul').children('.temperature').text('Temp: ' + data.list[timeInterval].main.temp + '°F');
+                forecastDay.children('ul').children('.wind').text('Wind: ' + data.list[timeInterval].wind.speed + 'MPH');
+                forecastDay.children('ul').children('.humidity').text('Humidity: ' + data.list[timeInterval].main.humidity + '%');
             }
 
         })
